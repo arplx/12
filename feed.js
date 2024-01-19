f3e47a6b3d9d44261322cc8924882bd55cf21391
arplx <93895162+arplx@users.noreply.github.com>
2024-01-20 04:43:53 +1200

    Update feed.js

--- feed.js
+++ feed.js
@@ -17,8 +17,8 @@ fetch('feed.json')
         <a class="timestamp" href="${product.timestamp}" _blank"="" style="font-size: 100px;text-decoration: none;color: white;margin-left: 40px;display: flex;margin-bottom: 5px;">
           <h2 style="opacity: 80%;font-size: 10px;padding: 0px;margin: 0px;">${product.time}</h2>
         </a>
-        <a <button="" href="${product.imageurl} type=" button"="" style="text-decoration: none;color: white;text-align: center;display: flow-root;width: -moz-available;padding-right: ;border-radius: 10px;">
-           <img alt="Image" style="margin-top: 10px;width: inherit;border-radius: 10px;object-fit: cover;" src="${product.image} "></a>
+        <a <button="" href="${product.imageurl} type=" button"="" style="text-decoration: none;color: white;text-align: center;display: flow-root;width: -moz-available;padding-right: ;border-radius: 10px;height: 500px;">
+           <img alt="Image" style="margin-top: 10px;width: inherit;border-radius: 10px;object-fit: cover;height: inherit;object-position: top;" src="${product.image} "></a>
         <p>${product.description}</p>
         <a <button="" href="${product.url} type=" button"="" style="padding: 0px;text-decoration: none;color: white;text-align: center;display: inline-flex;margin-top: auto;">
            <img alt="Image description" style="height: 30px;padding-right: 5px;margin-top: 17px;" src="https://i.postimg.cc/fbgg9FHp/ariaplus-blue-square-full-back1.jpg"> <p class="like-number" style="padding: 0px;margin: 0px;width: 100%;display: flex;margin-left: ;margin-top: 23px;font-weight: 600;font-size: 15px;"> ${product.likes} </p></a>
