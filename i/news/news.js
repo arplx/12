64d60f4ca5b5e0fa9dfae6ef2ee3da00b2e90d24
arplx <93895162+arplx@users.noreply.github.com>
2024-01-21 19:55:20 +1200

    Update news.js

--- i/news/news.js
+++ i/news/news.js
@@ -3,15 +3,15 @@ const newsContainer = document.getElementById('news-container');
 fetch('https://arplx.github.io/12/i/news/news.json')
   .then(response => response.json())
   .then(news => {
-    news.forEach(news => {
-      const trendElement = document.createElement('div');
-      newsElement.classList.add('news');
-      newsElement.innerHTML = `
-          <p class="trending-heading">${news.name}</p>
-          <p class="trending-text">${news.hastag}</p>
-          <p class="trending-heading">${news.numberposts}</p>
+    news.forEach(n => {
+      const nElement = document.createElement('div');
+      nElement.classList.add('trends');
+      nElement.innerHTML = `
+          <p class="trending-heading">${n.name}</p>
+          <p class="trending-text">${n.hastag}</p>
+          <p class="trending-heading">${n.numberposts}</p>
       `;
-      newsContainer.appendChild(newsElement);
+      newsContainer.appendChild(nElement);
     });
   })
   .catch(error => console.error('Error fetching news:', error));
